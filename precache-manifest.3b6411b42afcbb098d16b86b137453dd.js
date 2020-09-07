2fc875d5db6e4da4cb9e34b336202856d46ed7a4
Pablo Gonzalez <p.gonzalez@outlook.com>
2020-09-07 15:16:58 -0300

    Updates

---
+++ precache-manifest.3b6411b42afcbb098d16b86b137453dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27937f75b636eac869726e072d815e94",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "3ba858778c31622b8243",
+    "url": "/react-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "55687057fafa4a0b148d",
+    "url": "/react-calculator/static/js/2.96cc2af8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.96cc2af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba858778c31622b8243",
+    "url": "/react-calculator/static/js/main.460ad8ed.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-calculator/precache-manifest.21adbf93d7925a83aeecf75725316689.js"
+  "/react-calculator/precache-manifest.3b6411b42afcbb098d16b86b137453dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
