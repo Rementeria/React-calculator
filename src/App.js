0088e73490e867b9414ffe4acda874bfb84ed934
Pablo Gonzalez <p.gonzalez@outlook.com>
2020-09-09 15:38:09 -0300

    updated with css and styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import Calculator from "./components/Calculator";
-//import "./App.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="container text-center">
       <>
-        <h1 className="h1 mt-5">Calculator</h1>
-        <p className="lead">
-          Created to qualify for "Entry" position at 23People
-        </p>
+        <h1 className="h2 mt-5 tittle">Calculator</h1>
+        <p className="">Created to qualify for "Entry" position at 23People</p>
       </>
       <>
-        <Calculator initialValue={"0"} />
+        <Calculator initialValue={""} />
       </>
     </div>
   );
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,41 +16,9 @@ function Calculator({ initialValue }) {
       setPanel("0");
     }
   }, [initialValue]);
-  const numericButtons = () => {
-    const numbers = [];
-    for (let i = 9; i >= 0; i--) {
-      numbers.push(
-        <button
-          className="btn btn-outline-dark mx-2"
-          onClick={(e) =>
-            panel === "" || panel === "0"
-              ? setPanel(e.target.value)
-              : setPanel(panel + e.target.value)
-          }
-          value={i}
-          key={i}
-        >
-          {i}
-        </button>
-      );
-    }
-    return numbers;
+  const buttonNumber = (e) => {
+    panel === "" || panel === "0" ? setPanel(e) : setPanel(panel + e);
   };
-  const createFunction = (name, value) => {
-    return { name, value };
-  };
-
-  const functions = [
-    createFunction("AC", "AC"),
-    createFunction("<", "<"),
-    createFunction("Edit", "Edit"),
-    createFunction("+", "+"),
-    createFunction("-", "-"),
-    createFunction("*", "*"),
-    createFunction("/", "/"),
-    createFunction("(", "("),
-    createFunction(")", ")"),
-  ];
 
   const arithmeticCheck = (e) => {
     arithRegex.test(panel[panel.length - 1])
@@ -64,95 +32,166 @@ function Calculator({ initialValue }) {
     panel === "" || panel === "0" ? setPanel(e) : setPanel(panel + e);
   };
 
+  const createFunction = (name, value, style) => {
+    return { name, value, style };
+  };
+
+  const functions = [
+    createFunction("AC", "AC", "nes-btn is-error col-3"),
+    createFunction("<<", "<", "nes-btn is-warning col-3"),
+    createFunction("fix", "Edit", "nes-btn is-warning col-3"),
+
+    createFunction("+", "+", "nes-btn is-primary col-3"),
+    createFunction("-", "-", "nes-btn is-primary col-3"),
+    createFunction("*", "*", "nes-btn is-primary col-3"),
+
+    createFunction("/", "/", "nes-btn is-primary col-3"),
+    createFunction("(", "(", "nes-btn is-primary col-3"),
+    createFunction(")", ")", "nes-btn  is-primary col-3"),
+
+    createFunction("7", "7", "nes-btn col-3"),
+    createFunction("8", "8", "nes-btn col-3"),
+    createFunction("9", "9", "nes-btn col-3"),
+
+    createFunction("4", "4", "nes-btn col-3"),
+    createFunction("5", "5", "nes-btn col-3"),
+    createFunction("6", "6", "nes-btn col-3"),
+
+    createFunction("1", "1", "nes-btn col-3"),
+    createFunction("2", "2", "nes-btn col-3"),
+    createFunction("3", "3", "nes-btn col-3"),
+
+    createFunction("0", "0", "nes-btn col-3"),
+    createFunction(".", ".", "nes-btn col-3"),
+    createFunction("=", "=", "nes-btn is-success col-3"),
+  ];
+
+  const result = () => {
+    try {
+      setSubPanel(panel + "=");
+      setPanel(math.evaluate(panel));
+    } catch (err) {
+      setPanel("Error");
+      if (panel === "Error") {
+        setSubPanel(subPanel);
+      }
+    }
+  };
+
   return (
-    <div className="container center-text">
-      <div className="my-2">{subPanel}</div>
-      <div className="display-panel my-2">{panel}</div>
-      <div>
-        {functions.map((data, key) => (
-          <button
-            key={key}
-            value={data.value}
-            onClick={(e) => {
-              switch (data.value) {
-                case "AC":
-                  if (initialValue === "0" || initialValue === "") {
-                    setPanel("0");
-                    setSubPanel("0");
-                  } else {
-                    setPanel(initialValue);
-                    setSubPanel("0");
-                  }
-                  break;
-                case "<":
-                  if (panel.length > 1) {
-                    setPanel(panel.slice(0, -1));
-                  } else if (panel.length <= 1 && initialValue === "") {
-                    setPanel("0");
-                  } else if (panel.length <= 1 && initialValue !== "") {
-                    setPanel(initialValue);
-                  }
-                  break;
-                case "Edit":
-                  subPanel === "0"
-                    ? setPanel(panel)
-                    : setPanel(subPanel.slice(0, -2));
-                  break;
-                case "+":
-                  arithmeticCheck(e.target.value);
-                  break;
-                case "-":
-                  arithmeticCheck(e.target.value);
-                  break;
-                case "*":
-                  arithmeticCheck(e.target.value);
-                  break;
-                case "/":
-                  arithmeticCheck(e.target.value);
-                  break;
-                case "(":
-                  parenthesis(e.target.value);
-                  break;
-                case ")":
-                  parenthesis(e.target.value);
-                  break;
-              }
-            }}
+    <div className="calculator-size">
+      <div className="nes-container">
+        <div className="nes-input is-dark display">
+          <div className="mb-2 d-flex flex-row-reverse mr-3">{subPanel}</div>
+          <div className="d-flex flex-row-reverse mr-3 panel">{panel}</div>
+        </div>
+
+        <div className="container">
+          {functions.map((data, key) => (
+            <button
+              key={key}
+              value={data.value}
+              className={data.style}
+              onClick={(e) => {
+                switch (data.value) {
+                  case "AC":
+                    if (initialValue === "0" || initialValue === "") {
+                      setPanel("0");
+                      setSubPanel("0");
+                    } else {
+                      setPanel(initialValue);
+                      setSubPanel("0");
+                    }
+                    break;
+                  case "<":
+                    if (panel.length > 1) {
+                      setPanel(panel.slice(0, -1));
+                    } else if (panel.length <= 1 && initialValue === "") {
+                      setPanel("0");
+                    } else if (panel.length <= 1 && initialValue !== "") {
+                      setPanel(initialValue);
+                    }
+                    break;
+                  case "Edit":
+                    subPanel === "0"
+                      ? setPanel(panel)
+                      : setPanel(subPanel.slice(0, -1));
+                    break;
+                  case "+":
+                    arithmeticCheck(e.target.value);
+                    break;
+                  case "-":
+                    arithmeticCheck(e.target.value);
+                    break;
+                  case "*":
+                    arithmeticCheck(e.target.value);
+                    break;
+                  case "/":
+                    arithmeticCheck(e.target.value);
+                    break;
+                  case "(":
+                    parenthesis(e.target.value);
+                    break;
+                  case ")":
+                    parenthesis(e.target.value);
+                    break;
+                  case "1":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "2":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "3":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "4":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "5":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "6":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "7":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "8":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "9":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "0":
+                    buttonNumber(e.target.value);
+                    break;
+                  case ".":
+                    buttonNumber(e.target.value);
+                    break;
+                  case "=":
+                    result();
+                    break;
+                  default:
+                    setPanel("Error");
+                }
+              }}
+            >
+              {data.name}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="d-flex flex-row-reverse">
+        <p>
+          Style by:{" "}
+          <a
+            href="https://nostalgic-css.github.io/NES.css/?fbclid=IwAR0q7FJyFhkv8P9wOkr35zPE8MiQsPnZjqOqoGUn3s9THD2j2v0v2ajSaf4"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            {data.name}
-          </button>
-        ))}
-      </div>
-      <div className="numbers my-2">
-        <div>{numericButtons()}</div>
-        <button
-          className="btn btn-outline-dark mt-2"
-          onClick={(e) =>
-            panel === initialValue
-              ? setPanel(e.target.value)
-              : setPanel(panel + e.target.value)
-          }
-          value="."
-          key="."
-        >
-          .
-        </button>
-      </div>
-      <div className="result">
-        <button
-          className="btn btn-success"
-          onClick={() => {
-            try {
-              setSubPanel(panel + " =");
-              setPanel(math.evaluate(panel));
-            } catch (err) {
-              setPanel("Error, please press AC to delete or Prev to edit");
-            }
-          }}
-          value="="
-        >
-          =
-        </button>
+            NES.css
+          </a>
+        </p>
       </div>
     </div>
   );
